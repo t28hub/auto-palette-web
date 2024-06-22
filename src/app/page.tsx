ac320fd60a02ccd64730cef52785112a3910f060
Tatsuya Maki <t28oogle@gmail.com>
2024-06-22 14:39:30 +0900

    feat: add Installation section to Home (#9)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/card';
 import { Footer, Header } from '@/components/layouts';
 import { CodeIcon, CogIcon, PaletteIcon, PipetteIcon, SwatchIcon, WandIcon } from '@/components/icons';
@@ -7,37 +6,18 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-        <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-          <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-            Get started by editing&nbsp;
-            <code className='font-mono font-bold'>src/app/page.tsx</code>
+
+      <main className='flex min-h-screen flex-col items-center justify-between gap-4 p-24'>
+        <section className='w-full max-w-screen-lg flex items-center justify-center'>
+          <p className='flex w-full p-4 font-mono text-sm rounded-lg border-foreground bg-muted'>
+            [dependencies]
+            <br />
+            auto-palette = "0.4.0"
           </p>
-          <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none'>
-            <a
-              className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-              href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              By <Image src='/vercel.svg' alt='Vercel Logo' className='dark:invert' width={100} height={24} priority />
-            </a>
-          </div>
-        </div>
+        </section>
 
-        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-          <Image
-            className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-            src='/next.svg'
-            alt='Next.js Logo'
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <section className='w-full max-w-screen-lg grid items-start grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4'>
-          <Card>
+        <section className='w-full max-w-screen-lg grid grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4 items-stretch'>
+          <Card className='flex-grow'>
             <CardHeader>
               <WandIcon className='w-5 h-5 stroke-muted-foreground' strokeWidth={1.5} />
               <CardTitle>Automatic Palette Extraction</CardTitle>
@@ -48,7 +28,7 @@ export default function Home() {
               </p>
             </CardContent>
           </Card>
-          <Card>
+          <Card className='flex-grow'>
             <CardHeader>
               <PipetteIcon className='w-5 h-5 stroke-muted-foreground' strokeWidth={1.5} />
               <CardTitle>Detailed Color Insights</CardTitle>
