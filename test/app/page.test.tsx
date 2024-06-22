844bd6772139880735665582ed3c2fc10735d3b2
Tatsuya Maki <t28oogle@gmail.com>
2024-06-22 14:30:21 +0900

    feat: add Installation section to Home

--- test/app/page.test.tsx
+++ test/app/page.test.tsx
@@ -7,5 +7,10 @@ test('Page', () => {
   render(<Page />);
 
   // Assert
-  expect(screen.getByText('src/app/page.tsx')).toBeDefined();
+  expect(screen.getByText('Automatic Palette Extraction')).toBeDefined();
+  expect(screen.getByText('Detailed Color Insights')).toBeDefined();
+  expect(screen.getByText('Multiple Algorithms Support')).toBeDefined();
+  expect(screen.getByText('Theme-Based Color Selection')).toBeDefined();
+  expect(screen.getByText('Multiple Color Spaces Support')).toBeDefined();
+  expect(screen.getByText('Multiple Languages Support')).toBeDefined();
 });
