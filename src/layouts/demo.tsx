b057014269ca9584afb79f8187e4bc2c9b07a300
Tatsuya Maki <t28oogle@gmail.com>
2024-06-30 14:01:35 +0900

    feat(ui): maintain aspect ratio for bitmap images in DemoLayout

--- src/layouts/demo.tsx
+++ src/layouts/demo.tsx
@@ -1,11 +1,18 @@
 'use client';
 
-import { type FC, useEffect, useState } from 'react';
+import { type FC, useEffect, useRef } from 'react';
 import { BitmapImage } from '@/components/image';
-import { useBitmap, usePalette } from '@/hooks';
+import { useBitmap, useElementSize, usePalette } from '@/hooks';
 import { logger } from '@/utils';
+import { AspectRatio } from '@/components/ui';
 
+/**
+ *
+ * @constructor
+ */
 const DemoLayout: FC = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const containerSize = useElementSize(containerRef);
   const { bitmap, error } = useBitmap(
     'https://images.unsplash.com/photo-1718775971170-bab41e147a9f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NTc4MjR8MHwxfGFsbHw3fHx8fHx8Mnx8MTcxOTU2NzAwM3w&ixlib=rb-4.0.3&q=80&w=400',
   );
@@ -34,9 +41,13 @@ const DemoLayout: FC = () => {
 
   return (
     <section className='w-full min-w-screen-sm  max-w-screen-lg flex items-center justify-start'>
-      <div className='h-full w-full overflow-hidden rounded-lg bg-image-placeholder bg-4'>
-        {bitmap && <BitmapImage bitmap={bitmap} height={450} width={800} objectFit='cover' />}
-      </div>
+      <AspectRatio ref={containerRef} ratio={16 / 9} asChild className='flex items-center justify-center relative'>
+        <div className='h-full w-full overflow-hidden rounded-lg bg-image-placeholder bg-4'>
+          {bitmap && (
+            <BitmapImage bitmap={bitmap} height={containerSize.height} width={containerSize.width} objectFit='cover' />
+          )}
+        </div>
+      </AspectRatio>
     </section>
   );
 };
